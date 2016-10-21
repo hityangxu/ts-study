30cc23e3979a4b3f476041bda7d51cdabe853cbc
hityangxu <hityangxu@163.com>
2016-10-22 01:08:24 +0800

    extend interface

---
+++ src/interface/extend-interface.ts
@@ -0,0 +1,17 @@
+/**
+ * Created by yangxu on 16-10-22.
+ */
+
+interface Shape {
+    color: string;
+}
+
+interface PenStroke {
+    penWidth: number;
+}
+
+interface Square extends Shape, PenStroke {
+    sideLength: number;
+}
+
+let square = {} as Square;
