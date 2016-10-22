a1efd0613a32330473f74ebbe4d1bcf2eb0b3b83
hityangxu <hityangxu@163.com>
2016-10-23 00:19:39 +0800

    no overload

--- src/functions/function-type.ts
+++ src/functions/function-type.ts
@@ -9,6 +9,10 @@ let add: (x: number, y: number) => number = function(x: number, y:number) {
 let printNumber: (x: number) => void = function(x: number) {
     console.log(x);
 };
+//
+// let printNumber: (x: number, y: number) => void = function(x, y) {
+//     console.log(x, y);
+// };
 
 let minus: (x: number, y: number) => number = function(x, y) {
     return x - y;
