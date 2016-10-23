4ab338328da83f4bac98dc0c673c65d77269f3f8
hityangxu <hityangxu@163.com>
2016-10-23 21:12:29 +0800

    enum

---
+++ src/enum/initializer.ts
@@ -0,0 +1,13 @@
+/**
+ * Created by yangxu on 16-10-23.
+ */
+
+enum Direction {
+    UP = 1,
+    DOWN,
+    LEFT,
+    RIGHT
+}
+
+console.log(Direction.LEFT);
+console.log(Direction[Direction.LEFT]);
