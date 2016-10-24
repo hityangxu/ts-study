c4e84a1e3a5d0e29cf2e9b185141db40fd140e27
hityangxu <hityangxu@163.com>
2016-10-25 00:41:25 +0800

    use import to define an alias

---
+++ src/namespaces/test.ts
@@ -0,0 +1,14 @@
+///<reference path="validators.ts"/>
+///<reference path="NumberValidator.ts"/>
+
+/**
+ * Created by yangxu on 16-10-25.
+ */
+
+let letterValidator = new prs.Validation.LettersOnlyValidator();
+console.log(letterValidator.isAcceptable("123"));
+
+import validation = prs.Validation;
+
+let numberValidator = validation.NumberValidator();
+console.log(numberValidator.isAcceptable("321"));
